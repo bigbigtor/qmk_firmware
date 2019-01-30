5f5a043dfa4d8afe15cd4fb417c0b97687205713
victor <bigbigtor@users.noreply.github.com>
2019-01-30 09:21:09 +0100

    keymap update

--- keyboards/planck/keymaps/bigtor2/keymap.c
+++ keyboards/planck/keymaps/bigtor2/keymap.c
@@ -11,13 +11,13 @@ enum planck_layers {
 
 enum planck_keycodes {
   QWERTY = SAFE_RANGE,
-  NUMBER = LT(_NUMBER,KC_SPC),
+  NUMBER = LT(_NUMBER,KC_TAB),
   NAVIGA = LT(_NAVIGA,KC_ENT),
   FUNCTI = LT(_FUNCTI,KC_MNXT),
   CTLESC = CTL_T(KC_ESC), 
   SFTDEL = SFT_T(KC_BSPC),
   UNLOCK = LCA(KC_DEL),
-  ALTSPC = LALT_T(KC_SPC),
+  WINSPC = LGUI_T(KC_SPC),
   OSMSFT = OSM(MOD_LSFT),
 };
 
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV,  \
     CTLESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     OSMSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
-    UNLOCK,  KC_LGUI, KC_LALT, KC_RALT, NUMBER,  ALTSPC,  SFTDEL,  NAVIGA,  FUNCTI,  KC_VOLD, KC_VOLU, KC_MPLY  \
+    UNLOCK,  KC_RALT, KC_LALT, CTLESC,  NUMBER,  WINSPC,  SFTDEL,  NAVIGA,  FUNCTI,  KC_VOLD, KC_VOLU, KC_MPLY  \
 ),
 
 [_NUMBER] = LAYOUT_planck_grid(
